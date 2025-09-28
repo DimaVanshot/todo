b29a92920c8ff6b59f20121a68890d507c16e1e8
DimaVanshot <bakanovdima08052006@gmail.com>
2025-09-28 22:08:26 +0500

    first_commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+import express from 'express';
+
+import healthRouter from './routes/health.router';
+import todosRouter from './routes/todos.router';
+config(); // читает .env
+export function buildApp() {
+  const app = express();
+  app.use(express.json());
+  // Роуты
+  app.use('/health', healthRouter);
+  app.use('/api/todos', todosRouter);
+  // 404
+  app.use((req, res) => res.status(404).json({ error: 'Not Found' }));
+  // Ошибки
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (err: unknown, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    },
+  );
+  return app;
+}
